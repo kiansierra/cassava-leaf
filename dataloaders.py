0c50908aa0dcd38510618c43c8614ef36e6df238
kiansierra <kiansierra90@gmail.com>
2020-12-03 17:19:34 +0100

    Updated inference for batch and gpu

--- dataloaders.py
+++ dataloaders.py
@@ -2,7 +2,7 @@
 from typing import Tuple
 import os
 import pytorch_lightning as pl
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 from torchvision import transforms as tr
 from PIL import Image
 #%%
@@ -11,7 +11,7 @@ def load_image(image_path , to_tensor=False):
     if to_tensor: img = tr.ToTensor()(img)
     return img
 
-transform_list = [tr.RandomAffine(degrees=45), tr.RandomHorizontalFlip(p=0.5),  tr.RandomResizedCrop(size=(600,800), scale=(0.5,0.9))]
+transform_list = [tr.RandomAffine(degrees=15), tr.RandomHorizontalFlip(p=0.5),  tr.RandomResizedCrop(size=(600,800), scale=(0.5,0.9))]
 transformations = tr.Compose(transform_list)
 
 def collate_fun(transformations):
@@ -23,7 +23,7 @@ class CassavaDataset(Dataset):
     def __init__(self, df, data_dir = '..', transformations=None) -> None:
         super(CassavaDataset,self).__init__()
         self.df = df
-        self.data_dir = os.path.join(data_dir, 'train_images')
+        self.data_dir = data_dir
         self.transformations = transformations
     def __len__(self) -> int:
         return len(self.df)
@@ -37,21 +37,25 @@ class CassavaDataset(Dataset):
         return im, label
 # %%
 class CassavaDataModule(pl.LightningDataModule):
-    def __init__(self, train_df, val_df, test_df=None, batch_size=3, shuffle=True, data_dir = '..', num_workers=1) -> None:
+    def __init__(self, train_df, val_df, test_df=None, batch_size=3, sample_size=None, data_dir = '..', num_workers=1) -> None:
         super(CassavaDataModule,self).__init__()
         self.train_df = train_df
         self.val_df = val_df
         self.bs = batch_size
-        self.shuffle = shuffle
+        self.sample_size = sample_size
         self.data_dir = data_dir
         self.num_workers = num_workers
         self.test_df = test_df
-    def train_dataloader(self, *args, **kwargs) -> DataLoader:
-        train_dataset = CassavaDataset(self.train_df, data_dir=self.data_dir, transformations=transformations)
-        return DataLoader(train_dataset, batch_size=self.bs, shuffle = self.shuffle, num_workers=self.num_workers)
+    def train_dataloader(self, train=True, *args, **kwargs) -> DataLoader:
+        weights = 1/self.train_df.groupby('label').transform('count')['image_id']
+        train_dataset = CassavaDataset(self.train_df, data_dir=os.path.join(self.data_dir, 'train_images'), transformations=transformations)
+        sampler = None
+        if not self.sample_size: self.sample_size = len(train_dataset)
+        if train: sampler = WeightedRandomSampler(weights, num_samples=self.sample_size, replacement=True)
+        return DataLoader(train_dataset, batch_size=self.bs, num_workers=self.num_workers, sampler=sampler)
     def val_dataloader(self, *args, **kwargs) -> DataLoader:
-        val_dataset = CassavaDataset(self.val_df, data_dir=self.data_dir)
+        val_dataset = CassavaDataset(self.val_df, data_dir=os.path.join(self.data_dir, 'train_images'))
         return DataLoader(val_dataset, batch_size=self.bs, num_workers=self.num_workers)
     def test_dataloader(self, *args, **kwargs) -> DataLoader:
-        test_dataset = CassavaDataset(self.test_df, data_dir=self.data_dir)
+        test_dataset = CassavaDataset(self.test_df, data_dir=os.path.join(self.data_dir, 'test_images'))
         return DataLoader(test_dataset, batch_size=self.bs, num_workers=self.num_workers)
--- inference.py
+++ inference.py
@@ -1,10 +1,12 @@
 #%%
 from models import Resnet18, Resnet50, EfficientNetB1
-from dataloaders import load_image
 import torch
+import numpy as np
 import pandas as pd 
+from dataloaders import CassavaDataModule
 import os
 import argparse
+from tqdm import tqdm
 # %%
 def inference(args):
     classifier_list = [Resnet18, Resnet50, EfficientNetB1]
@@ -14,21 +16,45 @@ def inference(args):
     checkpoint_dir = os.path.join('logs', args.m)
     checkpoints = [elem for elem in os.listdir(checkpoint_dir) if elem.split('.')[-1] =='ckpt']
     checkpoint_path = os.path.join(checkpoint_dir, checkpoints[0])
-    model = classifier.load_from_checkpoint(checkpoint_path)
+    model = classifier.load_from_checkpoint(checkpoint_path).to('cuda')
     model.freeze()
-    df = pd.read_csv(os.path.join(args.dd, 'sample_submission.csv'))
-    TEST_DIR = os.path.join(args.dd, 'test_images')
-    for num in range(len(df)):
-        x = load_image(os.path.join(TEST_DIR, df.loc[num, 'image_id']), to_tensor=True)
-        df.loc[num, 'label'] = torch.argmax(model(x.unsqueeze(0))).cpu().numpy()
-    df.to_csv('submission.csv')
+    train_df = pd.read_csv(os.path.join(args.dd, 'train.csv'))
+    test_df = pd.read_csv(os.path.join(args.dd, 'sample_submission.csv'))
+    datamodule = CassavaDataModule(train_df, val_df=None, test_df=test_df, batch_size = 8, data_dir=args.dd, num_workers=4, shuffle=False)
+    preds = []
+    for batch in tqdm(iter(datamodule.test_dataloader())):
+        x, _ =batch
+        y_hat = model(x.to('cuda'))
+        preds += list(torch.argmax(y_hat, dim=1).cpu().numpy())
+    y_preds = np.array(preds)
+    print(y_preds.shape, len(test_df))
+    test_df['label'] = y_preds
+    test_df.to_csv('submission.csv', index=False)
     if args.tr:
-        df = pd.read_csv(os.path.join(args.dd, 'train.csv'))
-        TRAIN_DIR = os.path.join(args.dd, 'train_images')
-        for num in range(len(df)):
-            x = load_image(os.path.join(TRAIN_DIR, df.loc[num, 'image_id']), to_tensor=True)
-            df.loc[num, 'label_pred'] = torch.argmax(model(x.unsqueeze(0))).cpu().numpy()
-        df.to_csv('train_submission.csv')
+        preds = []
+        for batch in tqdm(iter(datamodule.train_dataloader(train=False))):
+            x, _ =batch
+            y_hat = model(x.to('cuda'))
+            preds += list(torch.argmax(y_hat, dim=1).cpu().numpy())
+        y_preds = np.array(preds)
+        print(y_preds.shape, len(train_df))
+        train_df['label_preds'] = y_preds
+        train_df.to_csv('train_submission.csv', index=False)
+    
+
+
+    # TEST_DIR = os.path.join(args.dd, 'test_images')
+    # for num in range(len(df)):
+    #     x = load_image(os.path.join(TEST_DIR, df.loc[num, 'image_id']), to_tensor=True)
+    #     df.loc[num, 'label'] = torch.argmax(model(x.unsqueeze(0))).cpu().numpy()
+    # df.to_csv('submission.csv')
+    # if args.tr:
+    #     df = pd.read_csv(os.path.join(args.dd, 'train.csv'))
+    #     TRAIN_DIR = os.path.join(args.dd, 'train_images')
+    #     for num in range(len(df)):
+    #         x = load_image(os.path.join(TRAIN_DIR, df.loc[num, 'image_id']), to_tensor=True)
+    #         df.loc[num, 'label_pred'] = torch.argmax(model(x.unsqueeze(0))).cpu().numpy()
+    #     df.to_csv('train_submission.csv')
 #%%
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
